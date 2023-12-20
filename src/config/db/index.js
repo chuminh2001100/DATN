ca079824f73bd8a92e5ead6ddfcbbcedfb9ed558
chuminh2001100 <chuminh2001100@gmail.com>
2023-12-20 22:35:56 +0700

    update test

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const serverSelectionTimeoutMS = 5000;
 async function connect(){
     try{
-        await mongoose.connect('mongodb://localhost:27017/caster_dev', {
+        await mongoose.connect('mongodb://127.0.0.1:27017/minh_cv', {
             serverSelectionTimeoutMS
         });
         console.log('Connect successfully!!!');
