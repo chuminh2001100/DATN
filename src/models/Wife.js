508258b446e8864a5c2b93c0f10e6922d8468715
chuminh2001100 <chuminh2001100@gmail.com>
2023-12-26 17:01:15 +0700

    plugin slug

--- src/models/Wife.js
+++ src/models/Wife.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+var slug = require('mongoose-slug-generator');
+mongoose.plugin(slug);
 const wife = new Schema({
     image: {type: String, maxLength: 1000},
     name: { type: String, maxLength: 255 },
     description: {type: String, maxLength: 600},
+    slug: { type: String, slug: "name" },
 },{
     timestamps: true,
 }
