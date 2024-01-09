96a37180a2878600688c7501afa614d92d04de04
chuminh2001100 <chuminh2001100@gmail.com>
2024-01-09 17:12:45 +0700

    update 2

--- src/controllers/CasterController.js
+++ src/controllers/CasterController.js
@@ -4,9 +4,13 @@ const dataPosition = require('../models/dataPosition');
 async function getDataRef(req, res){
     try {
         // Sử dụng await để đợi cho đến khi truy vấn hoàn tất
-        let result = await dataPosition.find().sort({ createdAt: -1 }).limit(1);
-        let result_2 = result[0].data;
-        res.send(result_2);
+        let result = await dataPosition.find().sort({ createdAt: -1 }).limit(2);
+        // let result_2 = result[0].data;
+        // res.send(result_2);
+        // res.send(result[1].data);
+        const concatenatedBuffer = Buffer.concat(result.map(doc => doc.data));
+        res.type('application/octet-stream');
+        res.send(concatenatedBuffer);
         console.log(`result: ${result}`);
     } catch (error) {
         // Xử lý lỗi nếu có
